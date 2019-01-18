fecf9d5715e0c012bb7340530540e443313797f0
Sonas MacRae <40277542@live.napier.ac.uk>
2019-01-18 01:58:20 +0000

    Fully working solution to project euler 14

--- Problem14.py
+++ Problem14.py
@@ -1,2 +1,28 @@
-# n/2 (even)
-# 3n + 1 (odd)
+def DetermineValue(x):
+    counter = 1
+
+    while x != 1:
+        if x % 2 == 0:
+             x = even(x)
+        else:
+            x = odd(x)
+        counter += 1
+        if x == 1:
+            return counter
+            break
+
+def odd(x):
+    return (3 * x) + 1
+
+def even(x):
+    return x / 2
+
+number = 0
+LAWD = 0
+
+for x in range(1, 1000000):
+    value = DetermineValue(x)
+    if value > number:
+        number = value
+        lawd = x
+print lawd
